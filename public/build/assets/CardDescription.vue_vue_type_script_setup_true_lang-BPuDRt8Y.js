a4281e3b6d60e9fcc50949f693b9191b29f3a72f
shafiqalshaar <shafiqalshaar@gmail.com>
2025-07-30 16:03:27 +0300

    wip

--- public/build/assets/CardDescription.vue_vue_type_script_setup_true_lang-BPuDRt8Y.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-CwJR0htU.js";import{d as t,a as r,o as n,n as c,e as p,M as l}from"./app-1KIabcXq.js";const f=t({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(a,m)=>(n(),r("p",{"data-slot":"card-description",class:c(p(o)("text-muted-foreground text-sm",e.class))},[l(a.$slots,"default")],2))}});export{f as _};
